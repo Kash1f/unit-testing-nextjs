b69b42a28b756cce6ac6a5bf3c764fd858f5cb22
Kashif <ahmed.kashif1001@gmail.com>
2024-10-31 00:10:28 +0500

    setup updates

---
+++ components/Home.test.tsx
@@ -0,0 +1,7 @@
+//Testing File for Home component. This will have unit testing for Home component
+
+//We might be having any particular utility function that takes two numbers and return us the sum of those numbers
+
+function sum(a:number, b:number){
+    return a + b;
+}
