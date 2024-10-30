fa54ea701c97b452dad7d30d0cd8127e049b3ebd
Kashif <ahmed.kashif1001@gmail.com>
2024-10-31 00:12:34 +0500

    unit test basics using jest with explanation

--- components/Home.test.tsx
+++ components/Home.test.tsx
@@ -2,6 +2,8 @@
 
 //We might be having any particular utility function that takes two numbers and return us the sum of those numbers
 
+//This takes two numbers and returns a sum
+//The testing here means if 2 and 3 are taken as numbers, then does it return 5? If yes then this function is added and is working fine. So, this is just one unit test, this is not the part of UI
 function sum(a:number, b:number){
     return a + b;
 }
